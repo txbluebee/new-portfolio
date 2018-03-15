580e9b7873eadfa33d5bbc6a52fdd65856a6d941
Brian Fan <kuomu.fan@yahoo.com>
2018-03-15 10:21:31 -0700

    add footer

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,17 +1,152 @@
-$(document).ready(function(){
-  var map = new GMaps({
-    div: '.map',
-    lat: 47.6185628,
-    lng: -122.2204679,
-    zoom: 12
+// Map
+var map;
+function initMap() {
+  map = new google.maps.Map(document.querySelector('.map'), {
+    center: { lat: 47.6223177, lng: -122.3021476 },
+    zoom: 13,
+    styles: [
+      {
+        "featureType": "landscape.man_made",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#f7f1df"
+          }
+        ]
+      },
+      {
+        "featureType": "landscape.natural",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#d0e3b4"
+          }
+        ]
+      },
+      {
+        "featureType": "landscape.natural.terrain",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "labels",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.business",
+        "elementType": "all",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.medical",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#fbd3da"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#bde6ab"
+          }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "geometry.stroke",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "labels",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [
+          {
+            "color": "#ffe15f"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+          {
+            "color": "#efd151"
+          }
+        ]
+      },
+      {
+        "featureType": "road.arterial",
+        "elementType": "geometry.fill",
+        "stylers": [
+          {
+            "color": "#ffffff"
+          }
+        ]
+      },
+      {
+        "featureType": "road.local",
+        "elementType": "geometry.fill",
+        "stylers": [
+          {
+            "color": "black"
+          }
+        ]
+      },
+      {
+        "featureType": "transit.station.airport",
+        "elementType": "geometry.fill",
+        "stylers": [
+          {
+            "color": "#cfb2db"
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#a2daf2"
+          }
+        ]
+      }
+    ]
   });
-  // add marker
-  map.addMarker({
-    lat: 47.6185628,
-    lng: -122.3504679,
-    title: 'Space Needle',
-    infoWindow: {
-      content: '<p>Famous Landmark</p>'
-    }
-  });
-})
+  var marker = new google.maps.Marker({
+    position: { lat: 47.620126, lng: -122.349358 },
+    map: map, // 指定地圖
+    title: 'Space Needle'
+  })
+}
+
+initMap();
